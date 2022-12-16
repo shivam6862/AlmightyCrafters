aef582feb69949b2f7291d64968631b18189b57e
Shivam kumar <115404926+shivam6862@users.noreply.github.com>
2022-12-16 22:32:36 +0530

    Update HomePerformace.js

--- src/components/Helper/Home/HomePerformace.js
+++ src/components/Helper/Home/HomePerformace.js
@@ -1,13 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 import classes from "./HomePerformance.module.css";
 const HomePerformance = () => {
+  const myref = useRef();
+  const [myElementIsVisible, setMyElementIsVisible] = useState();
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0];
+      setMyElementIsVisible(entry.isIntersecting);
+    });
+    observer.observe(myref.current);
+  }, []);
+
   const [totalitems, setTotalitems] = useState(0);
   const [totalOwner, settotalOwner] = useState(0);
   const [floorprice, setfloorprice] = useState(0.0001);
   const datato = 350;
+
+  if (myElementIsVisible === false && totalitems !== 0) {
+    setTotalitems(0);
+    settotalOwner(0);
+    setfloorprice(0.0001);
+  }
   const goToNext = () => {
-    const newtotalitem = totalitems + 1;
+    let newtotalitem = totalitems + 1;
     setTotalitems(newtotalitem);
   };
   const goToNext1 = () => {
@@ -21,12 +37,12 @@ const HomePerformance = () => {
     setfloorprice(newfloorprice);
   };
   useEffect(() => {
-    if (totalitems < datato) {
+    if (myElementIsVisible === true && totalitems < datato) {
       const interval = setInterval(() => {
         goToNext();
         goToNext1();
         goToNext2();
-      }, 5);
+      }, 1);
       return () => clearInterval(interval);
     }
   });
@@ -34,7 +50,7 @@ const HomePerformance = () => {
   return (
     <div className={classes.performance}>
       <div className={classes.factlist}>
-        <ul>
+        <ul ref={myref}>
           <li>
             <div className={classes.items}>
               <h3 className={classes.data}>
